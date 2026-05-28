71b07962a4f6d0f064800a70a98cf6d99fd76afd
Aran1992 <Aran1992@users.noreply.github.com>
2026-05-28 16:17:43 +0000

    deploy: 8dcce2900b73d6a98ffb8281ba8e047e0c989cf1

--- assets/BufferResource-pL4X_eaR.js
+++ assets/BufferResource-pL4X_eaR.js
@@ -1,4 +1,4 @@
-import{ap as U,a9 as g,S as c,an as S,d as _,e as m,E as I,am as h}from"./index-g1zf5i2W.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{ap as U,a9 as g,S as c,an as S,d as _,e as m,E as I,am as h}from"./index-BVgsOYcj.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
