a327b371708b4771ce7ae7de460fb9c892cd6295
Aran1992 <Aran1992@users.noreply.github.com>
2026-05-28 15:46:29 +0000

    deploy: f9877f3d3dab801f3e6f63f2c460c13f82391af9

--- assets/BufferResource-C1wiDvZ_.js
+++ assets/BufferResource-C1wiDvZ_.js
@@ -1,4 +1,4 @@
-import{ap as U,a9 as g,S as c,an as S,d as _,e as m,E as I,am as h}from"./index-D_JROWWx.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{ap as U,a9 as g,S as c,an as S,d as _,e as m,E as I,am as h}from"./index-BRusw8Kf.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
